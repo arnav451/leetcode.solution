c8d414a8997c236b74843cc8415e192f01edbfbf
arnav451 <arnavrathore451@gmail.com>
2026-08-19 15:09:22 +0530

    Time: 4 ms (16.53%), Space: 46.5 MB (10.15%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,21 +1,39 @@
+import java.util.*;
 class Solution {
-     void subset( int[]nums,int idx,List<Integer> curr,List<List<Integer>>  ans){
-        if(idx==nums.length){
-            ans.add(new ArrayList<>(curr));
+    public void func(List<Integer>output,List<Integer> input,List<List<Integer>> ans){
+
+        ans.add(new ArrayList<>(output));
+
+        if(input.size()==0){
             return;
         }
-        curr.add(nums[idx]);
-        subset(nums,idx+1,curr,ans);
-        curr.remove(curr.size()-1);
-        subset(nums,idx+1,curr,ans);
+        for(int i=0;i<input.size();i++){
+            List<Integer> ip = new ArrayList<>(input);
+            List<Integer> op = new ArrayList<>(output);
+            op.add(ip.get(i));
+            ip.subList(0,i+1).clear();
+            func(op,ip,ans);
         }
+
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-       List<List<Integer>> ans=new ArrayList<>();
-       List<Integer> curr = new ArrayList<>();
-       Set<List<Integer>> set = new HashSet<>(); 
-       Arrays.sort(nums);
-       subset(nums,0,curr,ans);
-       set.addAll(ans);
-       return new ArrayList<>(set) ;
+        List<Integer> input = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int a:nums){
+            input.add(a);
+
+        }
+        func(output,input,ans);
+
+
+        
+        HashSet<List<Integer>> set = new HashSet<>(ans);
+
+        List<List<Integer>> result = new ArrayList<>(set);
+
+        return result;
     }
 }
+
