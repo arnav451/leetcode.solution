aa430936811a61e8969a86b552fd95fd2bc1dce2
arnav451 <arnavrathore451@gmail.com>
2026-08-20 00:08:15 +0530

    Time: 2 ms (33.45%), Space: 46.6 MB (5.29%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    void perm(List<Integer> input,List<Integer> output,List<List<Integer>> ans){
+        if(input.size()==0){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+        for(int i=0;i<input.size();i++){
+        List<Integer> ip=new ArrayList<>(input);
+        List<Integer> op=new ArrayList<>(output);
+        op.add(ip.get(i));
+        ip.remove(i);
+        perm(ip,op,ans);
+    }
+    }
+
+    
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> input =new ArrayList<>();
+        List<Integer> output =new ArrayList<>(); 
+        for(int a:nums){
+            input.add(a);
+        }
+        perm(input,output,ans);
+        return ans;
+    }
+}
