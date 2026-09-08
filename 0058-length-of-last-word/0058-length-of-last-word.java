068124c6cef65e59eea4c2cf68bd440074449eba
arnav451 <arnavrathore451@gmail.com>
2026-09-08 18:02:25 +0530

    Time: 0 ms (100%), Space: 42.8 MB (92.32%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+     int count=0;
+     for(int i=s.length()-1;i>=0 ;i--){
+        if(count==0 && s.charAt(i)==' '){
+          continue;
+     }  
+        if(s.charAt(i)!=' '){
+            count++;
+        }else{
+            break;
+        }
+     }  
+     return count;
+    }
+}
