b0b520aaf9fa3f758666bacec4287166792fbe5b
arnav451 <arnavrathore451@gmail.com>
2026-09-10 10:25:20 +0530

    Time: 32 ms (5.94%), Space: 55 MB (11.22%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    HashMap<String, Integer> dp = new HashMap<>();
+
+    int fun(int i, String s, HashMap<String, Integer> m1, String tmp) {
+
+        if (i>=s.length()) {
+          if(tmp.length()==0) {
+                return 1;
+         }
+           return 0;
+        }
+          String key=i+"+"+tmp;
+
+        if(dp.containsKey(key)) {
+            return dp.get(key);
+     }
+        int m=0;
+
+        tmp += s.charAt(i);
+      if(m1.containsKey(tmp)) {
+            int a = fun(i + 1, s, m1, "");
+            m = m | a;
+     }
+        int a = fun(i + 1, s, m1, tmp);
+        m = m | a;
+        dp.put(key, m);
+        return m;
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+     HashMap<String, Integer> m1 = new HashMap<>();
+
+    for (String a : wordDict) {
+            m1.put(a, m1.getOrDefault(a, 0) + 1);
+        }
+        dp.clear();
+        return fun(0, s, m1, "") == 1;
+    }
+}
